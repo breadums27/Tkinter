1815ee23e21b963e8a16e2dcc4591ae0604520f7
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2017-01-04 11:17:07 -0800

    tkinter

---
+++ boi.py
@@ -0,0 +1,19 @@
+from Tkinter import * #gives us access to everything in the Tkinter class
+
+root = Tk() #gives us a blank canvas object to work with
+root.title("Corn")
+
+button1 = Button(root, text="Big Button")
+button1.grid(row=1, column=1)
+
+entry1 = Entry(root)
+entry1.grid(row=1, column=0)
+
+
+label1 = Label(root, text="Get Pranked by the Bradberry Boys", bg="red", anchor=W) 
+label1.grid(row=0, column=0, sticky=EW) 
+
+
+
+
+mainloop() #causes the windows to display on the screen until program closes
